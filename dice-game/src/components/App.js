ced0af48d39befb2b73c2fd0ff02c891394ba2bf
tahrer007 <tahrer_Ad@outlook.com>
2021-12-18 14:48:54 +0200

    update 14:48

--- dice-game/src/components/App.js
+++ dice-game/src/components/App.js
@@ -4,16 +4,35 @@ import GameBoard from "./gameboard/gameBoard";
 import Player from "./players/Player";
 
 class App extends React.Component {
-  
+  state = {
+    pointsTowin: 20,
+    dices: [null, null],
+    PlayerTurn: 1,
+    isWinner: false,
+    players: [
+      {
+        currentScores: 0,
+        totalScores: 0,
+      },
+      {
+        currentScores: 0,
+        totalScores: 0,
+      },
+    ],
+  };
+  rollDice = (childData) => {
+    this.setState({ dices: childData });
+  };
   componentDidMount = () => {};
   componentDidUpdate = () => {
     console.log("update!!!");
+    console.log(this.state.dices);
   };
   render() {
     return (
       <div className="mainContainer">
         <Player />
-        <GameBoard />
+        <GameBoard rollDice={this.rollDice} />
         <Player />
       </div>
     );
--- dice-game/src/components/gameboard/gameBoard.jsx
+++ dice-game/src/components/gameboard/gameBoard.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "./gamebord.css";
 
+const getRandomNumber = () => {
+  let min = 1;
+  let max = 7;
+  let random = Math.floor(Math.random() * (max - min) + min);
+
+  return random;
+};
+
 class gameBoard extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
+    console.log(props);
   }
+  state = {
+    random1: null,
+    random2: null,
+  };
+  rollDice = () => {
+    let random1 = getRandomNumber();
+    let random2 = getRandomNumber();
+    console.log(random1);
+    console.log(random2);
+
+    if (random1 === 6 && random2 === 6) {
+      this.props.rollDice([0, 0]);
+    } else this.props.rollDice([random1, random2]);
+  };
 
   componentDidMount = () => {};
   componentDidUpdate = () => {};
@@ -14,10 +37,12 @@ class gameBoard extends React.Component {
     return (
       <div className="gameBoard">
         <div className="newGame"></div>
-        <div className="dice"></div>
-        <div className="dice"></div>
+        <div className="dice face1"></div>
+        <div className="dice face1"></div>
         <div className="hold"></div>
-        <div className="roll"></div>
+        <div className="roll" onClick={this.rollDice}>
+          roll dice
+        </div>
       </div>
     );
   }
