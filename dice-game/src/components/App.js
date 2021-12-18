9cd8a8760a9d52d9739fb879994fb044e4c7ded3
tahrer007 <tahrer_Ad@outlook.com>
2021-12-18 22:09:36 +0200

    22:09

--- dice-game/src/components/App.js
+++ dice-game/src/components/App.js
@@ -4,24 +4,24 @@ import GameBoard from "./gameboard/gameBoard";
 import Player from "./players/Player";
 import { useState } from "react";
 
-const updatePlayers=(playersArr,playerNum,total)=>{
-
+const updatePlayers = (playersArr, playerNum, turnTotalScore) => {
   let UpdatePlayers = playersArr.map(function (player) {
     if (player.id === playerNum) {
-      player.totalScore = total;
+      player.totalScore += turnTotalScore;
       player.isTurn = false;
       return player;
     } else {
       player.isTurn = true;
-      return player};
+      return player;
+    }
   });
 
-  return UpdatePlayers ; 
-}
-const chengePlayer=(playerNum)=>{
-  if(playerNum===1)return 2 ; 
-  else return 1 ; 
-}
+  return UpdatePlayers;
+};
+const chengePlayer = (playerNum) => {
+  if (playerNum === 1) return 2;
+  else return 1;
+};
 
 class App extends React.Component {
   state = {
@@ -40,38 +40,29 @@ class App extends React.Component {
     ],
   };
 
-
-  changeTurn = (childData) => {
-    let playerNum = this.state.PlayerTurn;
-    let currentPlayer = this.state.players[playerNum];
-   // [playerNum,currentPlayer]=useState();
-
-   
-    console.log("currentPlayer : "+ currentPlayer)
-    let total = currentPlayer.totalScore;
-    total += childData;
-    console.log("child : " + total);
-    let UpdatePlayers = updatePlayers(this.state.players,playerNum,total) ; 
+  changeTurn = (turnTotalScore) => {
+    let UpdatePlayers = updatePlayers(
+      this.state.players,
+      this.state.PlayerTurn,
+      turnTotalScore
+    );
     this.setState({
       players: UpdatePlayers,
-      PlayerTurn:chengePlayer(playerNum),
+      PlayerTurn: chengePlayer(this.state.PlayerTurn),
     });
-
-    //console.log("the test"+this.state.players[this.state.PlayerTurn].totalScore);
   };
   componentDidMount = () => {};
   componentDidUpdate = () => {
-    console.log("update!!!");
+    /*console.log("update!!!");
     console.log(this.state.players);
-    console.log( "player turn : " + this.state.PlayerTurn);
+    console.log("player turn : " + this.state.PlayerTurn);*/
+    
   };
   render() {
     return (
       <div className="mainContainer">
         <Player playerData={this.state.players[0]} />
-        <GameBoard
-          changeTurn={this.changeTurn}
-        />
+        <GameBoard changeTurn={this.changeTurn} />
         <Player playerData={this.state.players[1]} />
       </div>
     );
