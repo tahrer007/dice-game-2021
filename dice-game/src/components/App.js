443eedf2c38391620237de715163978ca1156af7
tahrer007 <tahrer_Ad@outlook.com>
2021-12-18 18:12:29 +0200

    18:12

--- dice-game/src/components/App.js
+++ dice-game/src/components/App.js
@@ -10,39 +10,28 @@ class App extends React.Component {
     PlayerTurn: 1,
     isWinner: false,
     players: [
-      { id:1,
-        currentScore: 0,
-        totalScore: 0,
-        isTurn:false ,
-      },
+      { id: 1, currentScore: 0, totalScore: 0, isTurn: false },
       {
-        id:2,
+        id: 2,
         currentScore: 0,
         totalScore: 0,
-        isTurn:true ,
+        isTurn: true,
       },
     ],
   };
 
   changeTurn = (childData) => {
-    this.setState(
-      { PlayerTurn: childData });
+    this.setState({ PlayerTurn: childData });
   };
   rollDice = (childData) => {
-    this.setState({ dicesSum: childData ,  }
-      
-      
-      );
+    this.setState({ dicesSum: childData });
   };
   componentDidMount = () => {};
   componentDidUpdate = () => {
     console.log("update!!!");
     console.log(this.state.dicesSum);
-   
   };
   render() {
-
-   
     return (
       <div className="mainContainer">
         <Player playerData={this.state.players[0]} />
